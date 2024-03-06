6112bd05378ed9e0fc51fa47d57397ba09e7285c
ahmet <ahmet@CELAL>
2024-03-06 11:48:21 +0300

    Add project files.

---
+++ hw2/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using hw2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw2.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            FakeDB fakeDB = new FakeDB();
+            var allbooks = fakeDB.GetAllBooks();
+
+            return View(allbooks);
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var book = db.GetBookById(id.Value);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (!id.HasValue) return BadRequest();
+
+            FakeDB db = new FakeDB();
+            var result = db.DeleteBook(id.Value);
+            if (result)
+            {
+
+                return RedirectToAction("Indacex");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                FakeDB db = new FakeDB();
+                db.AddBook(book);
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+    }
+}
